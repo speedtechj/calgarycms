6d0ffc7d3dbd1d563011ef784bd30df3eb7c19aa
speedtechj <arbiasjr@gmail.com>
2023-06-22 11:35:22 -0600

    update

---
+++ database/migrations/2023_06_22_173058_add_discountfield_to__agentdiscounts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('_agentdiscounts', function (Blueprint $table) {
+            $table->foreignId('boxtype_id')->reference('id')->on('boxtypes')->constrained()->after('zone_id');
+            $table->boolean('is_active')->default(true)->after('boxtype_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('_agentdiscounts', function (Blueprint $table) {
+            $table->dropColumn('boxtype_id');
+            $table->dropColumn('is_active');
+        });
+    }
+};
