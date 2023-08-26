8b369a2b21bb07bfbee51c0cde3f008d88145cc7
speedtechj <arbiasjr@gmail.com>
2023-08-26 13:05:04 -0600

    update

--- database/migrations/2023_08_26_185940_add_paymentdate_to_bookings.php
+++ database/migrations/2023_08_26_185940_add_paymentdate_to_bookings.php
@@ -14,9 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('bookings', function (Blueprint $table) {
-            $table->foreignId('catextracharge_id')->after('note')->nullable()->constrained('catextracharges')->onDelete('cascade');
-            $table->bigInteger('extracharge_amount')->after('catextracharge_id')->nullable();
-            $table->boolean('box_replacement')->after('extracharge_amount')->default(0);
+            $table->date('payment_date')->nullable()->after('payment_balance');
         });
     }
 
@@ -28,13 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('bookings', function (Blueprint $table) {
-            $table->dropColumn('catextracharge_id');
-            $table->dropColumn('extracharge_amount');
-            $table->dropColumn('box_replacement');
+            $table->dropColumn('payment_date');
         });
     }
 };
-
-
-
-
