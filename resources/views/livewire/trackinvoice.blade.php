d37b1bc21fc7882009c15e805a6f6e3ae26dcde4
speedtechj <arbiasjr@gmail.com>
2023-09-27 16:09:46 -0600

    update

--- resources/views/livewire/trackinvoice.blade.php
+++ resources/views/livewire/trackinvoice.blade.php
@@ -1,8 +1,8 @@
 <div>
     <div class="w-full mb-10 text-lg font-bold">Generated/Manual Invoice</label>
-        <input wire:model="search"id="email-address" name="email" type="email" autocomplete="email" required
+        <input wire:model="search"id="invoice" name="email" type="text" autocomplete="email" required
             class="w-96 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2  shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-            placeholder="Enter your email">
+            placeholder="Enter your Invoice">
     </div>
 
 <div class="py-4 mx-10 mb-4 ">
