c6039549d085ba41ad976e8ef5f7b0571dd47047
speedtechj <arbiasjr@gmail.com>
2023-10-16 11:56:38 -0600

    update

--- app/Filament/Pages/Updatestatusinvoice.php
+++ app/Filament/Pages/Updatestatusinvoice.php
@@ -39,17 +39,17 @@ class Updatestatusinvoice extends Page implements HasTable, HasForms
     use Tables\Concerns\InteractsWithTable;
     use InteractsWithForms;
     use HasPageShield;
-  
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static ?string $navigationLabel = 'Edit/View Invoice';
-    
+
     public static ?string $label = 'Edit/View Invoice';
-    
+
     protected static ?string $navigationGroup = 'Invoice Status';
-   
+
     protected static string $view = 'filament.pages.manifest';
     public $isedit = true;
-   
+
     protected function getTableQuery(): Builder
     {
 
@@ -59,7 +59,6 @@ class Updatestatusinvoice extends Page implements HasTable, HasForms
     protected function getTableColumns(): array
     {
         return [
-
             Tables\Columns\TextColumn::make('generated_invoice')
                 ->label('Invoice')
                 ->searchable(isIndividual: true, isGlobal: false)
@@ -69,71 +68,74 @@ class Updatestatusinvoice extends Page implements HasTable, HasForms
                 ->label('Manual Invoice')
                 ->searchable(isIndividual: true, isGlobal: false)
                 ->sortable(),
-                Tables\Columns\TextColumn::make('trackstatus.description')
+            Tables\Columns\TextColumn::make('trackstatus.description')
                 ->label('Status')
-
                 ->sortable(),
-                Tables\Columns\TextColumn::make('date_update')
+            Tables\Columns\TextColumn::make('batch.batchno')
+                ->label('Batch Number')
+                ->sortable(),
+            Tables\Columns\TextColumn::make('date_update')
                 ->label('Status Date')
-               
+
                 ->sortable()
                 ->date('Y-m-d'),
             Tables\Columns\TextColumn::make('remarks'),
             Tables\Columns\TextColumn::make('boxtype.description')
                 ->label('Box Type')
-               
+
                 ->sortable(),
 
             Tables\Columns\TextColumn::make('sender.full_name')
                 ->label('Sender Name')
-               
+
                 ->sortable(),
             // ->url(fn (Booking $record) => route('filament.resources.senders.edit', $record->sender)),
             Tables\Columns\TextColumn::make('receiver.full_name')
                 ->label('Receiver')
-               
+
                 ->sortable(),
             // ->url(fn (Booking $record) => route('filament.resources.receivers.edit', $record->receiver)),
             Tables\Columns\TextColumn::make('provincephil.name')
                 ->label('Province')
-             
+
                 ->sortable(),
             Tables\Columns\TextColumn::make('cityphil.name')
                 ->label('City')
-               
+
                 ->sortable(),
-                Tables\Columns\TextColumn::make('location')
+            Tables\Columns\TextColumn::make('location')
                 ->label('Location')
                 ->searchable()
                 ->sortable(),
-                Tables\Columns\TextColumn::make('waybill')
+            Tables\Columns\TextColumn::make('waybill')
                 ->label('Waybill')
                 ->searchable()
                 ->sortable(),
-                
+
         ];
     }
     protected function getTableFilters(): array
     {
         return [
             SelectFilter::make('batch_id')
-            ->multiple()
-            ->options(Batch::all()->where('is_active', true)->pluck('batchno', 'id'))
+                ->multiple()
+                ->options(Batch::all()->where('is_active', true)->pluck('batchno', 'id'))
                 ->placeholder('Select Batch Number')
                 ->label('Batch Number')
-                ->default('0'),
-                SelectFilter::make('trackstatus_id')
+                ->default(array('Select Batch Number')),
+            SelectFilter::make('trackstatus_id')
+                ->multiple()
                 ->label('Status')
-                ->options(Trackstatus::all()->pluck('description', 'id'))
-                ->searchable()
-                ->default('1'),
+                ->options(Trackstatus::all()->where('branch_id', auth()->user()->branch_id)->pluck('description', 'id'))
+                ->searchable(),
+                // ->default(array('Select Status')),
             SelectFilter::make('provincephil_id')
                 ->label('Province')
                 ->options(Provincephil::all()->pluck('name', 'id'))
                 ->searchable(),
-                
-               
-            
+
+
+
         ];
     }
     protected function getTableFiltersLayout(): ?string
@@ -146,49 +148,55 @@ class Updatestatusinvoice extends Page implements HasTable, HasForms
     }
     protected function getTableActions(): array
     {
-       return[
-        Tables\Actions\DeleteAction::make()
-        ->label('Delete'),
-        Tables\Actions\Action::make('Edit Invoice Status')
-        ->label('Edit')
-        ->mountUsing(fn (Forms\ComponentContainer $form, Invoicestatus $record) => $form->fill([
-            'remarks' => $record->remarks,
-            'date_update' => $record->date_update,
-        ]))
-        ->form([
-            DatePicker::make('date_update')
+        return [
+            Tables\Actions\DeleteAction::make()
+                ->label('Delete'),
+            Tables\Actions\Action::make('Edit Invoice Status')
+                ->label('Edit')
+                ->mountUsing(fn (Forms\ComponentContainer $form, Invoicestatus $record) => $form->fill([
+                    'remarks' => $record->remarks,
+                    'date_update' => $record->date_update,
+                    'location' => $record->location,
+                    'waybill' => $record->waybill,
+                ]))
+                ->form([
+                    DatePicker::make('date_update')
                         ->label('Date Updated')
                         ->default(now())
                         ->closeOnDateSelection()
                         ->required(),
+                    Forms\Components\TextInput::make('location'),
+                    Forms\Components\TextInput::make('waybill'),
                     Forms\Components\Textarea::make('remarks')
-        ])
-        ->action(function (Invoicestatus $record, array $data): void {
-           
-            $record->update([
-                'date_update' => $data['date_update'],
-                'remarks' => $data['remarks'],
-            ]);
-        }),
-       ];
+                ])
+                ->action(function (Invoicestatus $record, array $data): void {
+
+                    $record->update([
+                        'date_update' => $data['date_update'],
+                        'remarks' => $data['remarks'],
+                        'location' => $data['location'],
+                        'waybill' => $data['waybill'],
+                    ]);
+                }),
+        ];
     }
     protected function getTableBulkActions(): array
     {
         return [
-    BulkAction::make('delete')
-    ->action(fn (Collection $records) => $records->each->delete()),
+            BulkAction::make('delete')
+                ->action(fn (Collection $records) => $records->each->delete()),
             BulkAction::make('Update Status')
                 ->action(function (Collection $records, array $data): void {
                     foreach ($records as $record) {
                         $record->update([
-                          
+
                             'date_update' => $data['date_updated'],
                             'remarks' => $data['remarks'],
                         ]);
                     }
                 })
                 ->form([
-                    
+
                     DatePicker::make('date_updated')
                         ->label('Date Stats Updated')
                         ->default(now())
@@ -198,14 +206,12 @@ class Updatestatusinvoice extends Page implements HasTable, HasForms
                 ])
         ];
     }
-    protected function getTableRecordsPerPageSelectOptions(): array 
+    protected function getTableRecordsPerPageSelectOptions(): array
     {
         return [10, 25, 50, 100];
-    } 
+    }
     // protected function shouldPersistTableFiltersInSession(): bool
     // {
     //     return true;
     // }
 }
-
-
--- app/Models/Invoicestatus.php
+++ app/Models/Invoicestatus.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoicestatus extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function batch():BelongsTo
+    {
+        return $this->belongsTo(Batch::class);
+    }
     public function booking()
     {
         return $this->belongsTo(Booking::class);
