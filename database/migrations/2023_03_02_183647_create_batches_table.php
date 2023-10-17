786863aac9b13ad29ecf052974a79ccd65442b1a
speedtechj <arbiasjr@gmail.com>
2023-10-17 13:47:55 -0600

    update

--- database/migrations/2023_03_02_183647_create_batches_table.php
+++ database/migrations/2023_03_02_183647_create_batches_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('batchno');
             $table->year('batch_year');
             $table->text('note')->nullable();
+            $table->boolean('is_active')->default('false');
+            $table->boolean('is_lock')->default('false');
             $table->timestamps();
         });
     }
