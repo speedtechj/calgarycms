799ea4da8d5d66e96a4f6a4a5d947f24f673bfa4
speedtechj <arbiasjr@gmail.com>
2023-10-17 02:21:22 -0600

    update

--- database/migrations/2023_10_17_081529_create_notifications_table.php
+++ database/migrations/2023_10_17_081529_create_notifications_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');
-            $table->nullableMorphs('notifiable');
-            $table->longText('data');
+            $table->morphs('notifiable');
+            $table->text('data');
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
