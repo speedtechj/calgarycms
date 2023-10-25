ce1a904b36b5c568466ed2052bc8e7fc1925b9da
speedtechj <arbiasjr@gmail.com>
2023-10-25 12:53:51 -0600

    update

---
+++ app/Exports/ManilamanifestExport.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Booking;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Illuminate\Database\Eloquent\Collection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+
+
+class ManilamanifestExport implements ShouldAutoSize, FromQuery, WithMapping, WithHeadings
+{
+    use Exportable;
+    public $booking;
+    
+    public function __construct(Collection $booking)
+    {
+       
+        $this->booking = $booking;
+    }
+    public function query()
+    {
+        return  Booking::wherekey($this->booking->pluck('id')->toArray());
+       
+        
+    }
+    public function map($booking): array
+    {
+        // dd($booking->senderaddress->citycan->name);
+        return [
+            $booking->booking_invoice,
+            $booking->manual_invoice,
+            '1',
+            $booking->boxtype->description,
+            $booking->batch->batchno.' '. '-' .' '.$booking->batch->batch_year,
+            $booking->sender->full_name,
+            $booking->receiver->full_name,
+            $booking->receiveraddress->address,
+            $booking->receiveraddress->barangayphil->name,
+            $booking->receiveraddress->cityphil->name,
+            $booking->receiveraddress->provincephil->name,
+            $booking->receiver->mobile_no,
+
+
+                    
+        ];
+    }
+    public function headings(): array
+    {
+        return [
+            'Generated Invoice',
+            'Manual Invoice',
+            'Quantity',
+            'Box Type',
+            'Batch Number',
+            'Sender Name',
+            'Receiver Name',
+            'Address',
+            'Barangay',
+            'City',
+            'Province',
+            'Mobile Number'     
+        ];
+    }
+}
---
+++ app/Filament/Pages/Manilamanifest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Filament\Pages;
+use Closure;
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Batch;
+use App\Models\Booking;
+use App\Models\Receiver;
+use Filament\Pages\Page;
+use App\Exports\CollectionExport;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\Layout;
+use App\Exports\ManilamanifestExport;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
+use Illuminate\Database\Eloquent\Model;
+use Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ReceiverResource;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Contracts\Pagination\Paginator;
+use Filament\Forms\Concerns\InteractsWithForms;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+
+class Manilamanifest extends Page implements HasTable, HasForms
+{
+    use Tables\Concerns\InteractsWithTable;
+    use InteractsWithForms;
+    use HasPageShield;
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationGroup = 'Report';
+    protected static string $view = 'filament.pages.manilamanifest';
+    public $isedit = true;
+    protected function getTableQuery(): Builder
+    {
+       
+         return Booking::query();
+    }
+ 
+    protected function getTableColumns(): array
+    {
+        return [
+            
+            Tables\Columns\TextColumn::make('booking_invoice')
+            ->label('Invoice')
+            ->searchable()
+            ->sortable(),
+            Tables\Columns\TextColumn::make('manual_invoice')
+            ->label('Manual Invoice')
+            ->searchable()
+            ->sortable(),
+            Tables\Columns\TextColumn::make('Quantity')
+            ->label('Quantity')
+            ->default('1'),
+            Tables\Columns\TextColumn::make('boxtype.description')
+            ->label('Box Type')
+            ->searchable()
+            ->sortable(),
+            Tables\Columns\TextColumn::make('batch.id')
+                    ->label('Batch No')
+                    ->sortable()
+                    ->searchable()
+                    ->getStateUsing(function (Model $record) {
+                        return $record->batch->batchno ."-". $record->batch->batch_year;
+                    }),
+            Tables\Columns\TextColumn::make('sender.full_name')
+            ->label('Sender Name')
+            ->searchable()
+            ->sortable()
+            ->url(fn (Booking $record) => route('filament.resources.senders.edit', $record->sender)),
+            Tables\Columns\TextColumn::make('receiver.full_name')
+            ->label('Receiver Name')
+            ->searchable()
+            ->sortable()
+    ->url(fn (Booking $record) => route('filament.resources.receivers.edit', $record->receiver)),
+            Tables\Columns\TextColumn::make('receiveraddress.address')
+            ->label('Address')
+            ->searchable()
+            ->sortable(),
+            Tables\Columns\TextColumn::make('receiveraddress.barangayphil.name')
+            ->label('Barangay')
+            ->searchable()->sortable(),
+            Tables\Columns\TextColumn::make('receiveraddress.provincephil.name')
+            ->label('Province')
+            ->searchable()->sortable(),
+            Tables\Columns\TextColumn::make('receiveraddress.cityphil.name')
+            ->label('City')
+            ->searchable()->sortable(),
+            Tables\Columns\TextColumn::make('receiver.mobile_no')
+            ->label('Mobile No')
+            ->searchable()->sortable(),
+            Tables\Columns\TextColumn::make('receiver.home_no')
+            ->label('Home No')
+            ->searchable()->sortable(),
+            
+        ];
+    } 
+    protected function getTableFilters(): array
+{
+    return [
+       
+    
+        SelectFilter::make('batch_id')
+        ->options(Batch::all()->where('is_active',true)->pluck('batchno','id'))
+        ->placeholder('Select Batch Number')
+        ->label('Batch Number')
+        ->default('0'),
+       
+    ];
+}
+protected function getTableFiltersLayout(): ?string
+{
+    return Layout::AboveContent;
+}
+protected function getTableFiltersFormColumns(): int
+{
+    return 3;
+}
+// protected function paginateTableQuery(Builder $query): Paginator
+protected function getTableBulkActions(): array
+{
+    return [
+
+        Tables\Actions\BulkAction::make('xls')->label('Export to Excel')
+                    ->icon('heroicon-o-document-download')
+                    ->action(fn (Collection $records) => (new ManilamanifestExport($records))->download('collection.xlsx')),
+            
+    ];
+}
+}
